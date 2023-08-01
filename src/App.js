de6a09401bd570ee1d9ed0e90f82390ed77e194a
Alexandr <log.koyd@gmail.com>
2023-08-01 11:01:54 +0300

    Added render list(map) from productArr.
    Signed-off-by: Alexandr <log.koyd@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,30 +1,42 @@
 import Card from './components/Card'
 import Header from "./components/Header";
 import RightCartBlock from "./components/RightCartBlock";
+
+
+const productArr = [
+    {title: 'Cap KYUTO Mid Mind', price: 7.999, imageUrl: '/img/product/Cap1.jpg'},
+    {title: 'White KYOTO Back Cap', price: 3.999, imageUrl: '/img/product/Cap2.jpg'},
+    {title: 'Country Travel STG', price: 15.999, imageUrl: '/img/product/Cap3.jpg'},
+    {title: 'Black Trip BN', price: 9.999, imageUrl: '/img/product/Cap4.jpg'},
+];
+
 function App() {
-  return (
-      <div className='wrapper clear'>
+    return (
+        <div className='wrapper clear'>
 
-    <RightCartBlock/>
-    <Header/>
+            <RightCartBlock/>
+            <Header/>
 
-          <div className='content p-40 '>
-              <div className='d-flex align-center mb-40 justify-between'>
-              <h1 >Все кепки</h1>
-                  <div className='search d-flex'>
-                      <img src='/img/search.svg' alt='Search'/>
-                      <input placeholder='Поиск...' type='text'/>
-                  </div>
-              </div>
-            <div className='d-flex'>
-          <Card/>
-          <Card/>
-          <Card/>
-          <Card/>
+            <div className='content p-40 '>
+                <div className='d-flex align-center mb-40 justify-between'>
+                    <h1>Все кепки</h1>
+                    <div className='search d-flex'>
+                        <img src='/img/search.svg' alt='Search'/>
+                        <input placeholder='Поиск...' type='text'/>
+                    </div>
+                </div>
+                <div className='d-flex'>
+                    {/* Рендер списка товара 01:23*/}
+                    {productArr.map((obj) => (
+                        <Card title={obj.title}
+                              price={obj.price}
+                              imageUrl={obj.imageUrl}/>
+                    ))}
+
+                </div>
             </div>
-          </div>
-      </div>
-  );
+        </div>
+    );
 }
 
 export default App;
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,22 +1,24 @@
-function Card () {
+function Card(props) {
+    console.log(props);
     return (
         <div className='card'>
-        <div className='favorite'>
-            <img src="/img/Hearth-prod.svg" alt=""/>
-        </div>
-        <img width={133} height={112} src='/img/Product/Cap1.jpg' alt='Cap'/>
-        <h5>Cap KYOTO Mid Mind</h5>
-        <div className='d-flex justify-between align-center'>
-            <div className='d-flex flex-column '>
-                <span>Цена</span>
-                <b>12.999 MDL</b>
+            <div className='favorite'>
+                <img src="/img/Hearth-prod.svg" alt=""/>
             </div>
+            <img width={133} height={112} style={{marginLeft: 15}} src={props.imageUrl} alt='Cap'/>
+            <h5>{props.title}</h5>
+            <div className='d-flex justify-between align-center'>
+                <div className='d-flex flex-column '>
+                    <span>Цена</span>
+                    <b>{props.price} MDL</b>
+                </div>
 
-            <button className='button'>
-                <img width={11} height={11} src='/img/Vector.svg' alt='Plus'/>
-            </button>
+                <button className='button'>
+                    <img width={11} height={11} src='/img/Vector.svg' alt='Plus'/>
+                </button>
+            </div>
         </div>
-    </div>
     );
 }
+
 export default Card;
